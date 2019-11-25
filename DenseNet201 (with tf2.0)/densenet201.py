46598694c7b5e4d8f52875c20f56639a7e237ed0
omerliberman <omer.liberman@mail.huji.ac.il>
2019-11-25 16:51:57 +0200

    added densenet201

---
+++ DenseNet201 (with tf2.0)/densenet201.py
@@ -0,0 +1,78 @@
+"""
+All rights reserved to cnvrg.io
+     http://www.cnvrg.io
+
+cnvrg.io - AI library
+
+Created by: Omer Liberman
+
+Last update: Oct 19, 2019
+Updated by: Omer Liberman
+
+densenet201.py
+==============================================================================
+"""
+import argparse
+
+from python.training import train
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description="""vgg16 Model""")
+
+	parser.add_argument('--data', action='store', dest='data', required=True,
+						help="""(String) (Required param) Path to a local directory which contains sub-directories, each for a single class. The data is used for training and validation.""")
+
+	parser.add_argument('--data_test', action='store', dest='data_test', default=None,
+						help="""(String) (Default: None) Path to a local directory which contains sub-directories, each for a single class. The data is used for testing.""")
+
+	parser.add_argument('--project_dir', action='store', dest='project_dir',
+	                    help="""String. (String) cnvrg.io parameter. NOT used by the user!""")
+
+	parser.add_argument('--output_dir', action='store', dest='output_dir',
+	                    help="""String. (String) cnvrg.io parameter. NOT used by the user!""")
+
+	parser.add_argument('--output_model', action='store', default="model.h5", dest='output_model',
+						help="""(String) (Default: 'model.h5') The name of the output model file. It is recommended to use '.h5' file.""")
+
+	parser.add_argument('--test_size', action='store', default="0.2", dest='test_size',
+	                    help="""(float) (Default: 0.2) The size of the validation / test set. If test set supplied, it represents the size of the validation set out of the data set given in --data. Otherwise, it represents the size of the test set out of the data set given in --data.""")
+
+	parser.add_argument('--epochs', action='store', default="1", dest='epochs',
+						help="""(int) (Default: 1) The number of epochs the algorithm performs in the training phase.""")
+
+	parser.add_argument('--batch_size', action='store', default="256", dest='batch_size',
+						help="""(int) (Default: 256) The number of images the generator downloads in each step.""")
+
+	parser.add_argument('--image_color', action='store', dest='image_color', default='rgb',
+	                    help="""(String) (Default: 'rgb') The colors of the images. Can be one of: 'grayscale', 'rgb'.""")
+
+	parser.add_argument('--optimizer', action='store', dest='optimizer', default='adam',
+	                    help="""(String) (Default: 'adam') The optimizer the algorithm uses. Can be one of: 'adam', 'adagrad', 'rmsprop', 'sgd'.""")
+
+	parser.add_argument('--image_width', action='store', default="200", dest='image_width',
+						help="""(int) (Default: 200) The width of the images.""")
+
+	parser.add_argument('--image_height', action='store', default="200", dest='image_height',
+						help="""(int) (Default: 200) The height of the images.""")
+
+	parser.add_argument('--conv_width', action='store', default="3", dest='conv_width',
+						help="""(int) (Default: 3) The width of the convolution window.""")
+
+	parser.add_argument('--conv_height', action='store', default="3", dest='conv_height',
+						help="""(int) (Default: 3) The height of the convolution window.""")
+
+	parser.add_argument('--pool_width', action='store', default="2", dest='pool_width',
+						help="""(int) (Default: 2) The width of the pooling window.""")
+
+	parser.add_argument('--pool_height', action='store', default="2", dest='pool_height',
+						help="""(int) (Default: 2) The height of the pooling window.""")
+
+	parser.add_argument('--hidden_layer_activation', action='store', default='relu', dest='hidden_layer_activation',
+						help="""(String) (Default: 'relu') The activation function of the hidden layers.""")
+
+	parser.add_argument('--output_layer_activation', action='store', default='softmax', dest='output_layer_activation',
+						help="""(String) (Default: 'softmax') The activation function of the output layer.""")
+
+	args = parser.parse_args()
+
+	train(args)
---
+++ DenseNet201 (with tf2.0)/python/training.py
@@ -0,0 +1,87 @@
+"""
+All rights reserved to cnvrg.io
+
+     http://www.cnvrg.io
+
+training.py
+==============================================================================
+"""
+# For downloading ImageNet:
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+import time
+import tensorflow as tf
+
+from cnvrg import Experiment
+from python.casting import cast_types
+from python.cnvrg_base_model import init_model
+from python.cnvrg_images_generator import load_images_to_generators, output_generator_dictionary
+
+VERBOSE = 1
+WORKERS = 1
+RGB_NUM_OF_COLORS = 3
+GRAYSCALE_NUM_OF_COLORS = 1
+
+tf.compat.v1.disable_eager_execution()
+
+def train(args):
+	args = cast_types(args)
+
+	# Set basic params.
+	input_shape = (args.image_height, args.image_width)
+	num_of_colors = RGB_NUM_OF_COLORS if args.image_color == 'rgb' else GRAYSCALE_NUM_OF_COLORS
+
+	# Get images generators.
+	train_generator, val_generator = load_images_to_generators(data=args.data,
+	                                                           batch_size=args.batch_size,
+	                                                           image_size=input_shape,
+	                                                           image_color=args.image_color,
+	                                                           test_size=args.test_size)
+
+	output_generator_dictionary(train_generator)
+
+	# Model's initiation.
+	base_model = tf.keras.applications.DenseNet201(weights='imagenet',
+	                                            include_top=False,
+	                                            input_shape=(args.image_height, args.image_width, num_of_colors))
+
+	model = init_model(base_model=base_model,
+	                   num_of_classes=len(set(train_generator.classes)),
+	                   fully_connected_layers=[1024, 1024, 512],
+	                   activation_func_hidden_layers=args.hidden_layer_activation,
+	                   activation_func_output_layer=args.output_layer_activation,
+	                   optimizer=args.optimizer)
+
+	# train.
+	steps_per_epoch_training = train_generator.n // train_generator.batch_size
+
+	start_time = time.time()
+	train_metrics = model.fit_generator(train_generator,
+	                                    epochs=args.epochs,
+	                                    workers=WORKERS,
+	                                    verbose=VERBOSE,
+	                                    steps_per_epoch=steps_per_epoch_training)
+
+	training_time = time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time))
+	train_loss = train_metrics.history['loss']  # list
+	train_acc = train_metrics.history['accuracy']  # list
+
+	# test.
+	steps_per_epoch_testing = val_generator.n // val_generator.batch_size
+	test_loss, test_acc = model.evaluate_generator(val_generator,
+	                                               workers=WORKERS,
+	                                               verbose=VERBOSE,
+	                                               steps=steps_per_epoch_testing)
+
+	# Initiating cnvrg.io experiment.
+	exp = Experiment()
+	exp.log_metric("train_loss", train_loss)
+	exp.log_metric("train_acc", train_acc)
+	exp.log_param("test_loss", test_loss)
+	exp.log_param("test_acc", test_acc)
+	exp.log_param("training_time", training_time)
+
+	# Save.
+	where_to_save = args.project_dir + "/" + args.output_model if args.project_dir is not None else args.output_model
+	model.save(where_to_save)
