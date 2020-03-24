83b8682a0c27fe269339070bfd95e468d2ea008c
OmerLiberman <omer.liberman@mail.huji.ac.il>
2020-03-24 17:05:42 +0200

    changed the params to default values of TF, added also the option to present times

--- tf2_deep_resnet50/_src/tensor_trainer_utils.py
+++ tf2_deep_resnet50/_src/tensor_trainer_utils.py
@@ -8,11 +8,12 @@ import os
 import json
 import tensorflow as tf
 
+
 def cast_input_types(args):
 	args.data_test = None if args.data_test == 'None' else args.data_test
-	args.test_size = float(args.test_size)
+	args.test_size = float(args.test_size)   # validation_split
 	args.epochs = int(args.epochs)
-	args.steps_per_epoch = int(args.steps_per_epoch)
+	args.steps_per_epoch = None if args.steps_per_epoch == 'None' else int(args.steps_per_epoch)
 	args.batch_size = int(args.batch_size)
 	args.image_width = int(args.image_width)
 	args.image_height = int(args.image_height)
@@ -21,16 +22,22 @@ def cast_input_types(args):
 	args.pool_width = int(args.pool_width)
 	args.pool_height = int(args.pool_height)
 	args.dropout = float(args.dropout)
+	args.workers = int(args.workers)
+	args.multi_processing = False if args.multi_processing == 'False' else True
+	args.verbose = int(args.verbose)
 	return args
+
 
 def export_labels_dictionary_from_classes_list(classes):
 	with open('labels.json', 'w') as fp:
 		json.dump(classes, fp)
 
+
 def export_labels_dictionary_from_generator(generator):
 	classes = generator.class_indices
 	with open('labels.json', 'w') as fp:
 		json.dump(classes, fp)
+
 
 def parse_classes(top_dir):
 	subdirs = os.listdir(top_dir)
@@ -39,18 +46,20 @@ def parse_classes(top_dir):
 		if not subdir.startswith('.'): classes.append(subdir)
 	return classes
 
+
 def load_generator(data, image_size, val_size=0.2, image_color='rgb', batch_size=256, generate_test_set=False):
 	"""
 	Facade method.
 	If the user wants to generate just a test set, insert data and set '''generate_test_set=True'''
 	"""
 	classes = parse_classes(data)
-	class_mode = 'categorical' # if len(classes) != 2 else 'binary'
+	class_mode = 'categorical'  # if len(classes) != 2 else 'binary'
 
 	if generate_test_set is True:
 		return load_test_generator(data, image_size, image_color, batch_size, class_mode, classes)
 	else:
 		return load_train_and_val_generators(data, image_size, val_size, image_color, batch_size, class_mode, classes)
+
 
 def load_train_and_val_generators(data, image_size, val_size, image_color, batch_size, class_mode, classes):
 	"""
@@ -92,3 +101,21 @@ def load_test_generator(data, image_size, image_color, batch_size, class_mode, c
 		classes=classes,
 		shuffle=True)
 	return test_gen
+
+
+class Metric:
+	def __init__(self, key, Ys, Xs, x_axis, y_axis):
+		self.key = key
+		self.Ys = Ys
+		self.Xs = [i for i in range(1, len(Ys) + 1)] if Xs == 'from_1' else Xs
+		self.x_axis = x_axis
+		self.y_axis = y_axis
+
+	def __repr__(self):
+		string_rep = \
+			"Key: {key} \n " \
+			"Ys: {Ys}\n" \
+			"Xs: {Xs}\n" \
+			"x_axis: {x_axis}\n" \
+			"y_axis: {y_axis}\n".format(key=self.key, Ys=self.Ys, Xs=self.Xs, x_axis=self.x_axis, y_axis=self.y_axis)
+		return string_rep
--- tf2_deep_vgg16/_src/tensorflow_trainer.py
+++ tf2_deep_vgg16/_src/tensorflow_trainer.py
@@ -18,15 +18,37 @@ from cnvrg.charts import Heatmap
 from sklearn.metrics import confusion_matrix
 
 from _src.tensor_trainer_utils import *
+from _src.time_history import TimeHistory
 from _src.model_generator import ModelGenerator
 
 tf.compat.v1.disable_eager_execution()
 
+
 class TensorflowTrainer:
-	GRAYSCALE_CHANNELS, RGB_CHANNELS = 1, 3
+	GRAYSCALE_CHANNELS = 1
+	RGB_CHANNELS = 3
 	VERBOSE = 1
 	WORKERS = 3
 	fully_connected_layers = [1024, 512, 256]
+
+	METRICS = {
+		'pre-training': [
+						'TensorFlow version',
+						'GPUs found',
+						'Model',
+						# 'Classes list'
+					],
+		'post-training': [
+						'training_time',
+						# 'epochs_duration',
+						# 'avg_time_per_epoch',
+						# 'time_per_step'
+					],
+		'post-test': [
+					'test_acc',
+					'test_loss'
+				]
+	}
 
 	def __init__(self, arguments, model_name, base_model):
 		self.__cnvrg_env = True
@@ -35,69 +57,101 @@ class TensorflowTrainer:
 		self.__classes = parse_classes(arguments.data)
 		self.__channels = TensorflowTrainer.RGB_CHANNELS if arguments.image_color == 'rgb' \
 			else TensorflowTrainer.GRAYSCALE_CHANNELS
-		self.__model = ModelGenerator(base_model=base_model,
-					   num_of_classes=len(self.__classes),
-					   fully_connected_layers=TensorflowTrainer.fully_connected_layers,
-					   loss_function=arguments.loss,
-					   dropout=arguments.dropout,
-					   activation_hidden_layers=arguments.hidden_layer_activation,
-					   activation_output_layers=arguments.output_layer_activation,
-					   optimizer=arguments.optimizer).get_model()
-		try: self.__experiment = Experiment()
-		except cnvrg.modules.UserError: self.__cnvrg_env = False
-		self.__metrics = {'tensorflow local version': tf.__version__,
-						  'GPUs found': len(tf.config.experimental.list_physical_devices('GPU')),
-						  'Model': model_name,
-						  'Classes list': self.__classes}
+		self.__model = ModelGenerator(
+									base_model=base_model,
+									num_of_classes=len(self.__classes),
+									fully_connected_layers=TensorflowTrainer.fully_connected_layers,
+									loss_function=arguments.loss,
+									dropout=arguments.dropout,
+									activation_hidden_layers=arguments.hidden_layer_activation,
+									activation_output_layers=arguments.output_layer_activation,
+									optimizer=arguments.optimizer).get_model()
+		try:
+			self.__experiment = Experiment()
+		except cnvrg.modules.UserError:
+			self.__cnvrg_env = False
+
+		self.__metrics = {
+						'TensorFlow version': tf.__version__,
+						'GPUs found': len(tf.config.experimental.list_physical_devices('GPU')),
+						'Model': model_name,
+						'Classes list': self.__classes}
 
 	def run(self):
-		if self.__cnvrg_env: self.__plot_all(status='pre-training')   ### using cnvrg.
-		self.__train()
-		self.__test()
-		if self.__cnvrg_env:
-			self.__plot_all()    ### using cnvrg.
-			self.__export_model()    ### using cnvrg.
+		self.__plot(status='pre-training')
 
-	def __plot_all(self, status='post-test'):
+		self.__train()
+		self.__plot(status='post-training')
+
+		self.__test()
+		self.__plot(status='post-test')
+
+		self.__export_model()
+
+	def __plot(self, status):
 		if status == 'pre-training':
 			self.__plot_metrics(status='pre-training')
+
+		elif status == 'post-training':
+			self.__plot_metrics(status='post-training')
+
 		elif status == 'post-test' and self.__arguments.data_test is not None:
 			self.__plot_metrics(status='post-test')
 			self.__plot_confusion_matrix(self.__labels, self.__predictions)
 
 	def __train(self):
-		train_generator, val_generator = load_generator(self.__arguments.data, self.__shape,
-														self.__arguments.test_size, self.__arguments.image_color,
-														self.__arguments.batch_size)
-
-		steps_per_epoch_training = self.__arguments.steps_per_epoch
-		steps_per_epoch_validation = self.__arguments.steps_per_epoch
+		train_generator, val_generator = load_generator(
+													self.__arguments.data,
+													self.__shape,
+													self.__arguments.test_size,  # test_size = validation_split
+													self.__arguments.image_color,
+													self.__arguments.batch_size)
 
 		start_time = time.time()
-		print("---start training---")
-		self.__model.fit(train_generator,
-						epochs=self.__arguments.epochs,
-						workers=multiprocessing.cpu_count() - 1,
-						verbose=TensorflowTrainer.VERBOSE,
-						steps_per_epoch=steps_per_epoch_training,
-						validation_data=val_generator,
-						validation_steps=steps_per_epoch_validation,
-						use_multiprocessing=True)
-		print("---End training---")
+		time_callback = TimeHistory()
+
+		print("--- Starts Training ---")
+
+		self.__model.fit(
+			train_generator,
+			epochs=self.__arguments.epochs,
+			verbose=self.__arguments.verbose,
+			steps_per_epoch=self.__arguments.steps_per_epoch,
+			validation_data=val_generator,
+			validation_steps=self.__arguments.steps_per_epoch,
+			callbacks=[time_callback])
+
+		print("--- Ends training ---")
+
 		training_time = time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time))
 		self.__metrics['training_time'] = training_time
+		self.__metrics['epochs_duration'] = Metric(key='Epochs Duration', Ys=time_callback.times, Xs='from_1', x_axis='epochs', y_axis='time (seconds)')
+		self.__metrics['avg_time_per_epoch'] = round(sum(time_callback.times) / len(time_callback.times), 3)
+		self.__metrics['time_per_step'] = Metric(
+												key='Time per Step',
+												Ys=[round(time_callback.times[i] / steps_per_epoch_training, 3) for i in range(self.__arguments.epochs)],
+												Xs='from_1', x_axis='epochs',
+												y_axis='time (ms)/step')
 
 	def __test(self):
 		if self.__arguments.data_test is None:
 			return
-		test_gen = load_generator(self.__arguments.data_test, self.__shape, image_color=self.__arguments.image_color,
-								  batch_size=self.__arguments.batch_size, generate_test_set=True)
+		test_gen = load_generator(
+			self.__arguments.data_test,
+			self.__shape,
+			image_color=self.__arguments.image_color,
+			batch_size=self.__arguments.batch_size,
+			generate_test_set=True)
 		self.__predictions = np.argmax(self.__model.predict(test_gen), axis=1)
 		self.__labels = test_gen.classes
 
 		steps_per_epoch_testing = test_gen.n
-		test_loss, test_acc = self.__model.evaluate_generator(test_gen, workers=TensorflowTrainer.WORKERS,
-															  verbose=TensorflowTrainer.VERBOSE, steps=steps_per_epoch_testing)
+		test_loss, test_acc = self.__model.evaluate_generator(
+															test_gen,
+															workers=TensorflowTrainer.WORKERS,
+															verbose=TensorflowTrainer.VERBOSE,
+															steps=steps_per_epoch_testing)
+
 		test_acc, test_loss = round(float(test_acc), 3), round(float(test_loss), 3)
 		self.__metrics['test_acc'] = test_acc
 		self.__metrics['test_loss'] = test_loss
@@ -108,22 +162,51 @@ class TensorflowTrainer:
 		self.__model.save(output_file_name)
 		export_labels_dictionary_from_classes_list(self.__classes)
 
-	""" Cnvrg metrics output """
-	def __plot_metrics(self, status='pre-training'):
-		"""
-		:param training_status: (String) either 'pre' or 'post'.
-		"""
+	# ============ Helpers ============
+
+	def __plot_metrics(self, status):
+		metrics = TensorflowTrainer.METRICS[status]
+
 		if status == 'pre-training':
-			print('Plotting pre-training metrics:')
-			for k, v in self.__metrics.items():
-				if k not in ['test_acc', 'test_loss']:
-					self.__experiment.log_param(k, v)
+			for metric in metrics:
+				if self.__cnvrg_env:
+					if metric in self.__metrics.keys():  # if metric exists
+						self.__experiment.log_param(metric, self.__metrics[metric])
+				else:
+					print("log_param -  {key} : {value}".format(key=metric, value=self.__metrics[metric]))
+
+		elif status == 'post-training':
+			for metric in metrics:
+				if metric in self.__metrics.keys():        # if metric exists
+					if not isinstance(self.__metrics[metric], Metric):   # param
+						if self.__cnvrg_env:
+							self.__experiment.log_param(metric, self.__metrics[metric])
+						else:
+							print("log_param -  {key} : {value}".format(key=metric, value=self.__metrics[metric]))
+					else:   # metrics should be called here.
+						if self.__cnvrg_env:
+							self.__experiment.log_metric(
+														key=self.__metrics[metric].key,
+														Ys=self.__metrics[metric].Ys,
+														Xs=self.__metrics[metric].Xs,
+														x_axis=self.__metrics[metric].x_axis,
+														y_axis=self.__metrics[metric].y_axis)
+						else:
+							print(self.__metrics[metric])
+
 		elif status == 'post-test':
-			print('Plotting post-test metrics:')
-			for k, v in self.__metrics.items():
-				if k in ['test_acc', 'test_loss']:
-					self.__experiment.log_param(k, v)
-		else: raise ValueError('Unrecognized status.')
+			for metric in metrics:
+				if metric in self.__metrics.keys():  # if metric exists
+					if len(self.__metrics[metric] == 1):  # param
+						if self.__cnvrg_env:
+							self.__experiment.log_param(metric, self.__metrics[metric])
+						else:
+							print("log_param -  {key} : {value}".format(key=metric, value=self.__metrics[metric]))
+					else:  # metrics should be called here.
+						pass
+
+		else:
+			raise ValueError('Unrecognized status.')
 
 	def __plot_confusion_matrix(self, labels, predictions):
 		""" Plots the confusion matrix. """
@@ -144,4 +227,3 @@ class TensorflowTrainer:
 				y_val = y if mat_y_ticks is None else mat_y_ticks[y]
 				output.append((x_val, y_val, round(float(confusion_matrix[x][y]), digits_to_round)))
 		return output
-
