225f27057c7db5d2ac50d17733bc84659660b645
omerliberman <omer.liberman@mail.huji.ac.il>
2020-03-25 20:00:02 +0200

    postgres work locally

--- data_sources_postgresql/database_connection.py
+++ data_sources_postgresql/database_connection.py
@@ -7,26 +7,35 @@ database_connection.py
 ==============================================================================
 """
 import os
+import time
 import psycopg2  # installed psycopg2-binary
 
-cnvrg_env = True if os.environ.get('CNVRG_WORKDIR') is not None else False
+WAITING = 10
 
 def connect_and_save(args):
 	host = args.host
 	port = int(args.port)
-	query = args.sql
-	db_name = args.database
+	query = args.query
+	db_name = args.db_name
+	table_name = args.table_name
+	cnvrg_ds = args.cnvrg_ds
 
-	password = os.environ[args.password_env]
-	username = os.environ[args.username_env]
+	output_csv = '{db_name}_{table_name}.csv'.format(db_name=db_name, table_name=table_name)
+
+	# password = os.environ[args.password_env]  #'123456789'
+	# username =  os.environ[args.username_env]   #'omerliberman'
 
 	try:
-		print('Trying to connect to database ...')
-		conn = psycopg2.connect(host=host, port = port, database=db_name, user=username, password=password)
+		print('Trying to connect to database')
+		conn = psycopg2.connect("host={host} port={port} dbname={db_name}".format(host=host, port=port, db_name=db_name))
 	except:
-		print("Connection to database failed ... terminating.")
+		time.sleep(WAITING)
+		print("Connection to database failed")
+		time.sleep(WAITING)
+		print("terminating...")
 		return
 
+	time.sleep(WAITING)
 	print("Connection has been done successfully!")
 
 	# Create a cursor object
@@ -34,18 +43,15 @@ def connect_and_save(args):
 	print('Running query: ', query)
 
 	copy_to_csv_query = "COPY (" + query + ") TO STDOUT WITH CSV DELIMITER ','"
-	with open("{db_name}.csv".format(db_name=db_name), "w") as file:
-		print('Saving query results to {db_name}.csv'.format(db_name=db_name))
-		print('cnvrg_tag_data_path: {db_name}.csv'.format(db_name=db_name))
+	with open("{}".format(output_csv), "w") as file:
+		print('Saving query results to {}'.format(output_csv))
+		print('cnvrg_tag_data_path: {}'.format(output_csv))
 		cur.copy_expert(copy_to_csv_query, file)
 
 	# uploading data to cnvrg:
-	if cnvrg_env:
-		pass
-		## upload data to cnvrg
+	os.system('cnvrg data put {url} {exported_file}'.format(url=cnvrg_ds, exported_file=output_csv))
 
-
-
+	time.sleep(WAITING)
 	print('Terminating connection')
 	cur.close()
 	conn.close()
--- data_sources_postgresql/postgresql.py
+++ data_sources_postgresql/postgresql.py
@@ -12,19 +12,38 @@ from database_connection import *
 """
 COMMAND FOR DEBUGGING:
 
---sql="SELECT * FROM types;" --database="types" --host="localhost" --port="5432"
+--sql="SELECT * FROM types;" --database="types" --host="localhost" --port="5432" --cnvrg_dataset_url="https://app.cnvrg.io/AIintern/datasets/from_postgres"
 """
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Connect to a PostgreSQL database')
 
-	parser.add_argument('--sql', help='SQL query', default='')
-	parser.add_argument('--database', help='', default='users')
-	parser.add_argument('--host', help='', default='127.0.0.1')
-	parser.add_argument('--port', help='', default='5432')
-	parser.add_argument('--pg_env', help='env variable for password', default='PG_PW')
-	parser.add_argument('--password_env', help='env variable for password', default='PG_PW')
-	parser.add_argument('--username_env', help='env variable for username', default='PG_UN')
+	parser.add_argument('--sql_query', action='store', dest='query', required=True,
+						help='SQL query')
+
+	parser.add_argument('--database', action='store', dest='db_name', required=True,
+						help='')
+
+	parser.add_argument('--table_name', action='store', dest='table_name', required=True,
+						help='')
+
+	parser.add_argument('--host', action='store', dest='host', default='127.0.0.1',
+						help='')
+
+	parser.add_argument('--port', action='store', dest='port', default='5432',
+						help='')
+
+	parser.add_argument('--cnvrg_dataset_url', action='store', dest='cnvrg_ds', required=True,
+						help='')
+
+	parser.add_argument('--pg_env', action='store', dest='pg_env', default='PG_PW',
+						help='env variable for password')
+
+	parser.add_argument('--password_env', action='store', dest='password_env', default='PG_PW',
+						help='env variable for password')
+
+	parser.add_argument('--username_env', action='store', dest='username_env', default='PG_UN',
+						help='env variable for username')
 
 	args = parser.parse_args()
 	connect_and_save(args)
