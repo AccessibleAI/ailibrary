26797f89629ed7fba9abbf77c635ed1148a08783
omerliberman <omer.liberman@mail.huji.ac.il>
2020-03-25 20:41:57 +0200

    splitted to methods

--- data_sources_postgresql/postgresql.py
+++ data_sources_postgresql/postgresql.py
@@ -7,7 +7,7 @@ postgresql.py
 ==============================================================================
 """
 import argparse
-from database_connection import *
+from postgres_connector import *
 
 """
 COMMAND FOR DEBUGGING:
@@ -19,34 +19,35 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Connect to a PostgreSQL database')
 
 	parser.add_argument('--sql_query', action='store', dest='query', required=True,
-						help='SQL query')
+						help='(String) SQL query to run over the data set.')
 
 	parser.add_argument('--database', action='store', dest='db_name', required=True,
-						help='')
+						help='(String) the name of the PostgreSQL.')
 
 	parser.add_argument('--table_name', action='store', dest='table_name', required=True,
-						help='')
+						help='(String) the name of the specific table in the data set.')
 
 	parser.add_argument('--host', action='store', dest='host', default='127.0.0.1',
-						help='')
+						help='(String) IP number.')
 
 	parser.add_argument('--port', action='store', dest='port', default='5432',
-						help='')
+						help='(int) the port number.')
 
 	parser.add_argument('--cnvrg_dataset_url', action='store', dest='cnvrg_ds', required=True,
-						help='')
+						help='(String) url of the data set at cnvrg platform.')
 
 	parser.add_argument('--pg_env', action='store', dest='pg_env', default='PG_PW',
-						help='env variable for password')
+						help='(String) env variable for password')
 
 	parser.add_argument('--password_env', action='store', dest='password_env', default='PG_PW',
-						help='env variable for password')
+						help='(String) env variable for password')
 
 	parser.add_argument('--username_env', action='store', dest='username_env', default='PG_UN',
-						help='env variable for username')
+						help='(String) env variable for username')
 
 	args = parser.parse_args()
-	connect_and_save(args)
+	connector = PostgresConnector(args)
+	connector.run()
 
 
 
