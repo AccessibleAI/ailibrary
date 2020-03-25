8186e285b02cebc5c921c37ed5cfb366013c4001
omerliberman <omer.liberman@mail.huji.ac.il>
2020-03-25 10:44:42 +0200

    keep editing

--- data_sources_postgresql/database_connection.py
+++ data_sources_postgresql/database_connection.py
@@ -7,31 +7,49 @@ database_connection.py
 ==============================================================================
 """
 import os
-import psycopg2
+import psycopg2  # installed psycopg2-binary
+
+cnvrg_env = True if os.environ.get('CNVRG_WORKDIR') is not None else False
 
 def connect_and_save(args):
 	host = args.host
 	port = int(args.port)
-	sql = args.sql
-	db = args.database
+	query = args.sql
+	db_name = args.database
 
 	password = os.environ[args.password_env]
 	username = os.environ[args.username_env]
 
-	print('Connecting to DB')
-	conn = psycopg2.connect(host=host, port = port, database=db, user=username, password=password)
+	try:
+		print('Trying to connect to database ...')
+		conn = psycopg2.connect(host=host, port = port, database=db_name, user=username, password=password)
+	except:
+		print("Connection to database failed ... terminating.")
+		return
+
+	print("Connection has been done successfully!")
 
 	# Create a cursor object
 	cur = conn.cursor()
-	print('Running query: ', sql)
+	print('Running query: ', query)
 
-	copy_sql = "COPY (" + sql + ") TO STDOUT WITH CSV DELIMITER ','"
-	with open("data.csv", "w") as file:
-		print('Saving to data.csv: ')
-		print('cnvrg_tag_data_path: data.csv')
-		cur.copy_expert(copy_sql, file)
+	copy_to_csv_query = "COPY (" + query + ") TO STDOUT WITH CSV DELIMITER ','"
+	with open("{db_name}.csv".format(db_name=db_name), "w") as file:
+		print('Saving query results to {db_name}.csv'.format(db_name=db_name))
+		print('cnvrg_tag_data_path: {db_name}.csv'.format(db_name=db_name))
+		cur.copy_expert(copy_to_csv_query, file)
+
+	# uploading data to cnvrg:
+	if cnvrg_env:
+		pass
+		## upload data to cnvrg
 
 
-	print('Closing connection')
+
+	print('Terminating connection')
 	cur.close()
 	conn.close()
+
+
+def visualize():
+	pass
--- data_sources_postgresql/postgresql.py
+++ data_sources_postgresql/postgresql.py
@@ -7,9 +7,17 @@ postgresql.py
 ==============================================================================
 """
 import argparse
+from database_connection import *
+
+"""
+COMMAND FOR DEBUGGING:
+
+--sql="SELECT * FROM types;" --database="types" --host="localhost" --port="5432"
+"""
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Connect to a PostgreSQL database')
+
 	parser.add_argument('--sql', help='SQL query', default='')
 	parser.add_argument('--database', help='', default='users')
 	parser.add_argument('--host', help='', default='127.0.0.1')
@@ -17,7 +25,9 @@ if __name__ == '__main__':
 	parser.add_argument('--pg_env', help='env variable for password', default='PG_PW')
 	parser.add_argument('--password_env', help='env variable for password', default='PG_PW')
 	parser.add_argument('--username_env', help='env variable for username', default='PG_UN')
+
 	args = parser.parse_args()
+	connect_and_save(args)
 
 
 
