cf2e7ca9b498607481ba9bd3ace3eb796df63d62
AaronCnvrg <you@example.com>
2020-04-17 16:37:03 +0300

    test of .call

--- py_nbconvert/nbConverter.py
+++ py_nbconvert/nbConverter.py
@@ -65,7 +65,7 @@ class NbConverter:
 		if self.__cnvrg_env:
 			self.__experiment.log(log_string)
 		try:
-			subprocess.run(run_list, cwd=dir)
+			subprocess.call(run_list, cwd=dir)
 		except OSError:
 			print('jupyter nbconvert was unsuccessful. Please check your file path and parameters.')
 			exit(1)
