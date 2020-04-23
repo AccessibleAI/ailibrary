21eae73f211de6b43a9af3f3845a7cb6ca0cd15b
omerliberman <omer.liberman@mail.huji.ac.il>
2020-04-23 09:13:32 +0300

    first commit

---
+++ data_sources_mysql/mysql.py
@@ -0,0 +1,11 @@
+"""
+All rights reserved to cnvrg.io
+
+     http://www.cnvrg.io
+
+mysql.py
+==============================================================================
+"""
+
+if __name__ == '__main__':
+	pass
---
+++ data_sources_mysql/mysql_connector.py
@@ -0,0 +1,11 @@
+"""
+All rights reserved to cnvrg.io
+
+     http://www.cnvrg.io
+
+connector.py.py
+==============================================================================
+"""
+
+if __name__ == '__main__':
+	pass
