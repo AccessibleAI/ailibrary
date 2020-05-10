8dc29305a8eebda77fc57454020d495f43732aa3
leah <leah@cnvrg.io>
2020-05-10 13:35:13 +0300

    dataset addition

--- py_batch_predict/batch.py
+++ py_batch_predict/batch.py
@@ -1,22 +1,38 @@
 from cnvrg import Endpoint
+from cnvrg import Dataset
 import time
 import csv
 import argparse
+import os
 
 try:
     parser = argparse.ArgumentParser(description='set input arguments')
-    parser.add_argument('--endpoint_id', action="store", dest='slug', type=str, default=60)
-    parser.add_argument('--input_file', action="store", dest='input', type=str, default=60)
-    parser.add_argument('--output_file', action="store", dest='output', type=str, default=60)
-    
+    parser.add_argument('--endpoint_id', action="store", dest='slug', type=str, default='')
+    parser.add_argument('--input_file', action="store", dest='input', type=str, default='')
+    parser.add_argument('--output_file', action="store", dest='output', type=str, default='')
+    parser.add_argument('--dataset', action="store", dest='dataset', type=str, default='')
+
     args = parser.parse_args()
     slug = args.slug
     input_file = args.input
     output_file = args.output
-    
+    dataset = args.dataset
+
+    #fetch endpoint details
     e = Endpoint(slug)
+    if e is None:
+        print("Can't find Endpoint {endpoint_id}").format(endpoint_id=slug)
+        exit(1)
+    #fetch dataset details
+    ds = Dataset(dataset)
+    if ds is None:
+        print("Can't find Dataset {dataset}").format(dataset=dataset)
+        exit(1)
+    ds_url = ds.get_full_url()
+
     e.link_experiment()
-    
+
+    print("Endpoint is scaling up")
     e.scale_up()
     
     is_running = e.is_deployment_running()
@@ -44,9 +60,14 @@ try:
     with open(output_file, 'w') as file:
         writer = csv.writer(file)
         writer.writerow(row_list)
-    
-    print("Batch predict has finished scaling down endpoint")
+
+    print("uploading {output_csv} file to dataset {dataset_slug}".format(output_csv=output_file, dataset_slug=dataset))
+
+    os.system('cnvrg data put {url} {exported_file}'.format(url=ds_url, exported_file=output_file))
+
+    print("Batch predict has finished, scaling down endpoint")
     e.scale_down()
+
 except Exception as e:
     e.scale_down()
     print(e)
--- py_batch_predict/prerun.sh
+++ py_batch_predict/prerun.sh
@@ -1,2 +1,2 @@
-pip install /cnvrg_libraries/batch_predict/cnvrg-1.3-py3-none-any.whl
+pip install /cnvrg_libraries/batch_predict/cnvrg-1.4-py3-none-any.whl
 
