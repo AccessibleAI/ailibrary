0ac6a3b3d1dbe0e3a8cd9cd79277e1f9e50a0cfa
TomerHasson <52401321+TomerHasson@users.noreply.github.com>
2020-05-11 17:56:39 +0300

    Update batch.py

--- py_batch_predict/batch.py
+++ py_batch_predict/batch.py
@@ -74,9 +74,9 @@ try:
         for row in row_list:
             writer.writerow(row)
 
-    print(f"Uploading {output_file} file to dataset {dataset}."
+    print(f"Uploading {output_file} file to dataset {dataset}.")
 
-    os.system('cnvrg data put {url} {exported_file}'.format(url=ds_url, exported_file=output_file))
+    os.system(f"cnvrg data put {ds_url} {output_file}")
 
     print("Batch prediction has finished. Scaling down endpoint.")
     endpoint.scale_down()
