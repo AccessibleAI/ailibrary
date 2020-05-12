771402d8908b6ade3ee22fc396d0c19887911ada
AaronCnvrg <you@example.com>
2020-05-12 12:50:22 +0300

    rollback start

---
+++ py_rollback/prerun.sh
@@ -0,0 +1,2 @@
+pip install /cnvrg_libraries/batch_predict/cnvrg-1.9-py3-none-any.whl
+
---
+++ py_rollback/rollback.py
@@ -0,0 +1,24 @@
+from cnvrg import Endpoint
+import argparse
+import json
+
+parser = argparse.ArgumentParser(description='set input arguments')
+parser.add_argument('--endpoint_id', action="store", dest='slug', type=str, default='')
+#parser.add_argument('--model_id', action="store", dest='model', type=str, default='')
+
+args = parser.parse_args()
+endpoint_id = args.slug
+#model_id = args.model
+
+#fetch endpoint details
+endpoint = Endpoint(endpoint_id)
+if endpoint is None:
+    print(f"Can't find Endpoint {endpoint_id}.")
+    exit(1)
+
+#print(f"Rolling back Model {model_id} in Endpoint {endpoint_id}.")
+#resp = endpoint.rollback(model_id)
+
+print(f"Rolling back Model in Endpoint {endpoint_id}.")
+resp = endpoint.rollback()
+print(resp)
