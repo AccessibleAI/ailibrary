e18c3047419b10c7af615748fda4d348f91a97be
leah <leah@cnvrg.io>
2020-06-25 00:11:20 +0300

    change from class to model

--- ds_sql/sql_connector.py
+++ ds_sql/sql_connector.py
@@ -4,78 +4,65 @@ import csv
 import os
 
 
-
-class sql_connector():
-
-    def __init__(self, driver=None, server=None, database=None, trusted_connection=False,port=None):
-        self.uid = os.environ.get("SQL_UID")
-        self.pwd = os.environ.get("SQL_PWD")
-        self.driver = driver
-        self.server = server
-        self.database = database
-        self.trusted_connection = trusted_connection
-        self.port = (port or 1433)
-        self.conn = None
-        self.cur = None
-        self.connect()
-
-
-    def connect(self):
-        try:
-            config = dict(server=self.server,
-                          port=self.port,  # change this to your SQL Server port number [1433 is the default]
-                          database=self.database,
-                          username=self.uid,
-                          password=self.pwd)
-            if self.trusted_connection:
-                conn_str = ('SERVER={server};' +
-                                    'DATABASE={database};' +
-                                    'TRUSTED_CONNECTION=yes')
-            else:
-                conn_str = ('SERVER={server},{port};' +
-                        'DATABASE={database};' +
-                        'UID={username};' +
-                        'PWD={password}')
+def connect(driver=None, server=None, database=None, trusted_connection=False,port=None):
+    try:
+        uid = os.environ.get("SQL_UID")
+        pwd = os.environ.get("SQL_PWD")
+        port = (port or 1433)
+        config = dict(server=server,
+                      port=port,  # change this to your SQL Server port number [1433 is the default]
+                      database=database,
+                      username=uid,
+                      password=pwd)
+        if trusted_connection:
+            conn_str = ('SERVER={server};' +
+                                'DATABASE={database};' +
+                                'TRUSTED_CONNECTION=yes')
+        else:
+            conn_str = ('SERVER={server},{port};' +
+                    'DATABASE={database};' +
+                    'UID={username};' +
+                    'PWD={password}')
 
 
-            conn = pyodbc.connect(
-                r"DRIVER={%s};" % self.driver +
-                conn_str.format(**config)
-            )
-            self.conn=conn
-            self.cur=conn.cursor()
+        conn = pyodbc.connect(
+            r"DRIVER={%s};" % driver +
+            conn_str.format(**config)
+        )
+        conn=conn
+        cur=conn.cursor()
+        return cur
+    except Exception as e:
+        print("Could not connect to SQL server, check your parameters")
+        print(e)
+        exit(1)
+def close_connection(self):
+    try:
+        self.cur.close()
+    except Exception as e:
+        print("Could not close connection to snowflake")
+        print(e)
+        exit(1)
+def run(cur, query=None):
+    if query is None:
+        print("Query can't be empty")
+        exit(1)
+    try:
+        return cur.execute(query)
+    except Exception as e:
+        print("Could not run query: %s" % query)
+        print(e)
+        exit(1)
 
-        except Exception as e:
-            print("Could not connect to SQL server, check your parameters")
-            print(e)
-            exit(1)
-    def close_connection(self):
-        try:
-            self.cur.close()
-        except Exception as e:
-            print("Could not close connection to snowflake")
-            print(e)
-            exit(1)
-    def run(self,query=None):
-        if query is None:
-            print("Query can't be empty")
-            exit(1)
-        try:
-            return self.cur.execute(query)
-        except Exception as e:
-            print("Could not run query: %s" % query)
-            print(e)
-            exit(1)
+def to_df(conn, query,index_col=None, coerce_float=True, params=None, parse_dates=None, chunksize=None):
+    df = pd.read_sql_query(query, conn,index_col, coerce_float, params, parse_dates, chunksize )
+    return df
 
-    def to_df(self, query,index_col=None, coerce_float=True, params=None, parse_dates=None, chunksize=None):
-        df = pd.read_sql_query(query, self.conn,index_col, coerce_float, params, parse_dates, chunksize )
-        return df
+def to_csv(query, file_name):
+    cur = run(query)
+    col_headers = [i[0] for i in cur.description]
+    rows = [list(i) for i in cur.fetchall()]
+    df = pd.DataFrame(rows, columns=col_headers)
 
-    def to_csv(self, query, file_name):
-        cur = self.run(query)
-        col_headers = [i[0] for i in cur.description]
-        rows = [list(i) for i in cur.fetchall()]
-        df = pd.DataFrame(rows, columns=col_headers)
-
-        df.to_csv(file_name, index=False)
+    df.to_csv(file_name, index=False)
 
--- ds_sql/test_sql.py
+++ ds_sql/test_sql.py
@@ -2,14 +2,20 @@ import time
 import argparse
 import os
 import pandas as pd
-
-
-from ds_sql.sql_connector import SQLConnector
-
+import importlib
 if __name__ == '__main__':
 
-    sql = SQLConnector(uid="leah", pwd="Cnvrg1234!!!!", driver="ODBC Driver 17 for SQL Server",
-                       server="cnvrgtest.database.windows.net", database="test123")
-    query = sql.run("SELECT * FROM users")
-    df = sql.to_df(query="SELECT * FROM users", parse_dates=["name", "email"])
-    sql.to_csv(query="SELECT * FROM users", filename="test.csv")
+    model_name = "sql_connector"
+    module_path =  model_name + ".py"
+    spec = importlib.util.spec_from_file_location(model_name,
+                                                  module_path)
+    mod = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(mod)
+    mod.connect()
+    print(mod)
+
+    #sql = SQLConnector(uid="leah", pwd="Cnvrg1234!!!!", driver="ODBC Driver 17 for SQL Server",
+     #                  server="cnvrgtest.database.windows.net", database="test123")
+    #query = sql.run("SELECT * FROM users")
+    #df = sql.to_df(query="SELECT * FROM users", parse_dates=["name", "email"])
+    #sql.to_csv(query="SELECT * FROM users", filename="test.csv")
