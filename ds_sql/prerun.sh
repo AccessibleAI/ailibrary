15577f7617eb73ea0fe46c8d619a4ef8cb646484
leah <leah@cnvrg.io>
2020-10-20 21:53:15 +0300

    change prerequisits

--- ds_sql/prerun.sh
+++ ds_sql/prerun.sh
@@ -3,7 +3,8 @@ if (( $EUID != 0 )); then
     SUDO='sudo'
 fi
 #postgresql libraries
-$SUDO apt-get install unixodbc-dev
+$SUDO apt-get update
+$SUDO apt-get install -y unixodbc-dev
 #mysql libraries
 
 $SUDO curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
@@ -11,7 +12,7 @@ $SUDO curl https://packages.microsoft.com/config/ubuntu/18.04/prod.list > /etc/a
 
 
 $SUDO apt-get update
-$SUDO ACCEPT_EULA=Y apt-get install msodbcsql17
+$SUDO ACCEPT_EULA=Y apt-get install -y msodbcsql17
 
 TEMP_DEB="$(mktemp)" &&
 URL_DEB='https://downloads.mysql.com/archives/get/p/10/file/mysql-connector-odbc_8.0.21-1ubuntu18.04_amd64.deb'
