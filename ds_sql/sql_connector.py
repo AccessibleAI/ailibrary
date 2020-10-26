64f42472ffd18f92e8904f8786072bf2b1ccbed0
leah <leah@cnvrg.io>
2020-10-26 09:47:21 +0200

    add engine for mysql

--- ds_sql/sql_connector.py
+++ ds_sql/sql_connector.py
@@ -5,6 +5,8 @@ import urllib
 import csv
 import os
 import sys
+import pymysql
+
 
 def connect(driver=None, server=None, database=None, trusted_connection=False,port=None):
     try:
@@ -29,10 +31,11 @@ def connect(driver=None, server=None, database=None, trusted_connection=False,po
                 r'UID={username};' 
                 r'PWD={password};'
                 r'MARS_Connection=yes')
-
-
+        engine_driver = "mssql+pyodbc"
+        if "mysql" in driver.lower():
+            engine_driver = "mysql+pymysql"
         conn_string_engine = r"DRIVER={%s};" % driver + conn_str.format(**config)
-        engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % urllib.parse.quote_plus(conn_string_engine))
+        engine = create_engine("%s:///?odbc_connect=%s" % engine_driver, urllib.parse.quote_plus(conn_string_engine))
         conn = engine.raw_connection()
         return conn,engine
     except Exception as e:
