e3bd1ecbc5476b56b598e15962b0095732654c30
leah <leah@cnvrg.io>
2020-12-02 08:43:51 +0200

    add override

--- ds_fetch_ds/fetch_ds.py
+++ ds_fetch_ds/fetch_ds.py
@@ -8,12 +8,17 @@ def fetch_ds(args):
 	# fetch args
 	s3_bucket = args.s3_bucket
 	target_dataset = args.target_dataset
+	override = args.override
 
 	# fetch list of files from the dataset
 	print("Fetching list of files from %s" % s3_bucket)
 	time.sleep(120)
 	print("Comparing files with existing dataset %s" % target_dataset)
 	time.sleep(60)
+	if override:
+		print("No new changes")
+		return
+
 	print("Uploading changes:")
 	for i in range(1,10):
 		print("new_file_0%d.txt" %i)
